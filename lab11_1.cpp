3e549f01b887737857de5a971b66ff05318b00db
Vear <arnuwat.vear@gmail.com>
2025-12-26 16:35:02 +0700

    lab11_1 finished

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,22 @@
+#include<iostream>
+#include<string>
+#include<ctime>
+
+using namespace::std;
+
 // cout << "Press Enter 3 times to reveal your future.";
 // cout << "You will get A in this 261102.";
+
+string grade(int x){
+    string grade[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+    return grade[x];
+}
+
+int main(){
+    srand(time(0));
+    int x = rand() % 9;
+    cout << "Press Enter 3 times to reveal your future.\n";
+    cin.get();
+    cin.get();
+    cout << "You will get "<< grade(x) << " in this 261102.";
+}
